19a9380cdd8651d8ccf972786454baaac0cc03ac
Felipe <garciasuezfelipe@gmail.com>
2022-05-24 09:01:22 -0300

    Homework 01

--- 01-IntroToCS/homework/homework.js
+++ 01-IntroToCS/homework/homework.js
@@ -2,12 +2,32 @@
 
 function BinarioADecimal(num) {
   // tu codigo aca
-
+  
+  let decimal = 0
+  let binaryArray = String(num).split("").map((num)=>{
+    return Number(num)
+  })
+  for(let i = 0; i< binaryArray.length; i++){
+  decimal = decimal + binaryArray[i] * 2 ** (binaryArray.length - 1 - i)
+  }
+  return decimal
+  
+  
 }
 
 function DecimalABinario(num) {
   // tu codigo aca
 
+  let binario = []
+  while(num>=1){
+    if(num % 2 == 0){
+      binario.unshift("0")
+    } else {
+        binario.unshift("1")
+    }
+    num = num / 2
+  }
+  return binario.join("")
 }
 
 
